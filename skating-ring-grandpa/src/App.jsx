ef8033ec568ea87e41f5e72a2c0ce53708686e72
Vonney <davonnejv@gmail.com>
2023-07-07 18:29:01 -0500

    adding heading, trying to get background image

--- skating-ring-grandpa/src/App.jsx
+++ skating-ring-grandpa/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Genes Roller Rama</h1>
+    <div> 
+     Genes Roller Rama
+    </div>
     </>
   )
 }
